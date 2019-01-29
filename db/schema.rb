0c822cba888dc0f6f36237b9022c4d83047d9220
devastian1 <kevindevastian.com>
2019-01-29 12:18:42 +0800

    added access level for different users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_29_025834) do
+ActiveRecord::Schema.define(version: 2019_01_29_041306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2019_01_29_025834) do
     t.string "remember_token", limit: 128
     t.string "avatar"
     t.string "password_digest"
+    t.integer "access_level", default: 0
     t.index ["email"], name: "index_users_on_email"
     t.index ["remember_token"], name: "index_users_on_remember_token"
   end
